aec209faaf550280d0dbc5f186f1cdeb8cf70c3c
U-BSD\497022 <497022@SHS-8YBF6X3.beaverton.k12.or.us>
2025-09-05 10:24:35 -0700

    Its really not working at all :(

--- StudentList.cpp
+++ StudentList.cpp
@@ -9,37 +9,50 @@ struct Student {
   char lastName[20];
   int ID;
   float GPA;
-
 };
 
-Student addStudent();
-
+void addStudent(vector<Student*> &s);
+void printStudents(vector<Student*> s);
 
 int main() {
 
   vector<Student*> students;
 
-  Student greg = addStudent();
+  char input[20];
 
-  students.push_back(&greg);
+  do {
+    cout << endl << "Enter command (type \"HELP\" for all commands):" << endl; 
+    cin.get(input, 20);
+    cin.clear();
+    cin.ignore(10000, '\n');
+    cout << endl;
+
+    if (strcmp(input, "HELP") == 0) {
+      cout << "List of commands:" << endl;
+      cout << " - ADD: Add a new student to the database." << endl;
+      cout << " - PRINT: Prints out all of the students in the database." << endl;
+      cout << " - DELETE: Remove a student from the database." << endl;
+      cout << " - QUIT: Close the program" << endl;
+    }
+    else if (strcmp(input, "ADD") == 0) {
+      addStudent(students);
+    }
+    else if (strcmp(input, "PRINT") == 0) {
+      printStudents(students);
+    }
+    else if (strcmp(input, "REMOVE") == 0) {
+
+    }
+    
+  } while (strcmp(input, "QUIT") != 0);
   
-  cout << (*students.at(0)).firstName << endl;
-  cout << greg.lastName << endl;
-  cout << greg.ID << endl;
-  cout << greg.GPA << endl;
   
-
-
   
-
-
-
-
 
   return 0;
 }
 
-Student addStudent() {
+void createStudent(vector<Student*> &s) {
   Student newS;
   cout <<"Adding a new student..." << endl;
   cout << "Enter first name:" << endl;
@@ -56,5 +69,13 @@ Student addStudent() {
   cout << "Enter GPA:" << endl;
   cin >> newS.GPA;
 
-  return newS;
+  s.push_back(&newS);
+}
+
+void printStudents(vector<Student*> s) {
+  cout << "Complete list of students:" << endl;
+
+  for (int i = 0; i < s.size(); i++) {
+    cout << s.at(i) -> firstName << " " << s.at(i) -> lastName << ", " << s.at(i) -> ID << ", " << s.at(i) -> GPA << endl;
+  }
 }

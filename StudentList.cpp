e4605ec565e59978cba91f4af2e7c99eb98a336d
U-BSD\497022 <497022@SHS-8YBF6X3.beaverton.k12.or.us>
2025-09-08 10:24:18 -0700

    nearly complete, just make GPA round propperly

--- StudentList.cpp
+++ StudentList.cpp
@@ -13,21 +13,24 @@ struct Student {
 
 void addStudent(vector<Student*> &s);
 void printStudents(vector<Student*> s);
+void removeStudent(vector<Student*> &s);
 
 int main() {
 
   vector<Student*> students;
-
   char input[20];
 
+  //This will run until the user enters "QUIT"
   do {
+    //ask for user input
     cout << endl << "Enter command (type \"HELP\" for all commands):" << endl; 
     cin.get(input, 20);
     cin.clear();
     cin.ignore(10000, '\n');
     cout << endl;
-
+    
     if (strcmp(input, "HELP") == 0) {
+      //show all the valid commands
       cout << "List of commands:" << endl;
       cout << " - ADD: Add a new student to the database." << endl;
       cout << " - PRINT: Prints out all of the students in the database." << endl;
@@ -35,13 +38,16 @@ int main() {
       cout << " - QUIT: Close the program" << endl;
     }
     else if (strcmp(input, "ADD") == 0) {
+      //add a student
       addStudent(students);
     }
     else if (strcmp(input, "PRINT") == 0) {
+      //show the complete list of students
       printStudents(students);
     }
-    else if (strcmp(input, "REMOVE") == 0) {
-
+    else if (strcmp(input, "DELETE") == 0) {
+      //remove a student
+      removeStudent(students);
     }
     
   } while (strcmp(input, "QUIT") != 0);
@@ -52,30 +58,71 @@ int main() {
   return 0;
 }
 
-void createStudent(vector<Student*> &s) {
-  Student newS;
+void addStudent(vector<Student*> &s) {
+  Student* sPtr = new Student();
   cout <<"Adding a new student..." << endl;
   cout << "Enter first name:" << endl;
-  cin.get(newS.firstName, 20);
+  cin.get((*sPtr).firstName, 20);
   cin.get();
 
   cout << "Enter last name:" << endl;
-  cin.get(newS.lastName, 20);
+  cin.get((*sPtr).lastName, 20);
   cin.get();
 
-  cout << "Enter student ID:" << endl;
-  cin >> newS.ID;
+  //gets student id and verifies that its valid
+  do {
+    if (cin.fail()) {
+      cin.clear();
+      cin.ignore(10000, '\n');
+    }
+    cout << "Enter student ID:" << endl;
+    cin >> (*sPtr).ID;
+  } while (cin.fail() || (*sPtr).ID <= 0);
 
-  cout << "Enter GPA:" << endl;
-  cin >> newS.GPA;
+  
+  do {
+    if (cin.fail()) {
+      cin.clear();
+      cin.ignore(10000, '\n');
+    }
+    cout << "Enter GPA:" << endl;
+    cin >> (*sPtr).GPA;
+  } while (cin.fail() || (*sPtr).GPA < 0 || (*sPtr).GPA > 4.5);
 
-  s.push_back(&newS);
+  cin.clear();
+  cin.ignore(10000, '\n');
+  
+  s.push_back(sPtr);
 }
 
 void printStudents(vector<Student*> s) {
   cout << "Complete list of students:" << endl;
 
+  //prints out each student's complete information
   for (int i = 0; i < s.size(); i++) {
     cout << s.at(i) -> firstName << " " << s.at(i) -> lastName << ", " << s.at(i) -> ID << ", " << s.at(i) -> GPA << endl;
+  }
+}
+
+void removeStudent(vector<Student*> &s) {
+  //asks for ID and saves it
+  int id = 0;
+  do {
+    if (cin.fail()) {
+      cin.clear();
+      cin.ignore(10000, '\n');
+    }
+    cout << "Enter the student ID of the student to remove:" << endl;
+    cin >> id;
+  } while (cin.fail());
+
+  //removes ALL students who have that ID
+  for (int i = 0; i < s.size(); i++) {
+    if ((*(s.at(i))).ID == id) {
+      //deletes student and gets rid of pointer
+      delete s.at(i);
+      s.erase(s.begin() + i);
+      i--;
+    }
   }
 }
